a0bd83f37090a2a2ab4f0113bea90b0a0015ebd4
Ronan <1254198616@qq.com>
2020-11-23 16:20:31 +0800

    完成下拉框组件

--- src/components/Select/style.js
+++ src/components/Select/style.js
@@ -1,6 +1,22 @@
 import styled from "styled-components";
+import CaretDown from "assets/icon/caret_down.svg"
 
-const StyledSelect = styled.select``;
+const StyledSelect = styled.select`
+/* 除去默认下拉图标 */
+  appearance: none;
+  ::-ms-expand{
+    display:none;
+  }
+  background-image:url(${CaretDown});
+  background-position:right center;
+  background-repeat:no-repeat;
+  background-color:transparent;
+  padding-right:14px;
+  border:none;
+  font-size:${({theme}) => theme.normal};
+  color:${({theme}) => theme.grayDark};
+  
+`;
 
 export default StyledSelect;
 
